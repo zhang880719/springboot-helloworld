5a633e5b85bd5dc0bbeac1bf158522b3403777c3
zdj <380056768@qq.com>
2022-05-15 21:36:47 +0800

    first commit

--- src/main/java/com/bjsxt/springboothelloworld/controller/HelloWorldController.java
+++ src/main/java/com/bjsxt/springboothelloworld/controller/HelloWorldController.java
@@ -1,10 +1,12 @@
 package com.bjsxt.springboothelloworld.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class HelloWorldController {
 	
 	@Value("${msg}")
@@ -12,6 +14,8 @@ public class HelloWorldController {
 
 	@RequestMapping("/hello")
 	public String showMsg(){
+	    String str="日志编码utf-8";
+		log.info("测试日志:{}",str);
 		return this.msg;
 	}
 
